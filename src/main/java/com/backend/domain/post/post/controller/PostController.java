6a805f7eb6dd70b6827e7da8170721ba6b9d51c1
woongcheol Na <32201345@dankook.ac.kr>
2025-10-06 03:57:54 +0900

    postController의 고객입력 html을 메서드로 정리, 재사용

--- src/main/java/com/backend/domain/post/post/controller/PostController.java
+++ src/main/java/com/backend/domain/post/post/controller/PostController.java
@@ -15,11 +15,9 @@ public class PostController {
 
     private final PostService postService;
 
-    @GetMapping("/posts/write")
-    @ResponseBody
-    public String write(){
-
+    private String getWriteFormHtml(String errorMessage){
         return """
+                <div style="color:red">%s</div>
                 <form method="POST" action="http://localhost:8080/posts/doWrite">
                   <input type="text" name="title">
                   <br>
@@ -27,7 +25,14 @@ public class PostController {
                   <br>
                   <input type="submit" value="작성">
                 </form>
-                """;
+                """.formatted(errorMessage);
+    }
+
+    @GetMapping("/posts/write")
+    @ResponseBody
+    public String write(){
+
+        return getWriteFormHtml("");
     }
 
     //POST 요청으로 받는다
@@ -37,6 +42,10 @@ public class PostController {
             String title,
             String content
     ){
+
+        if(title.isBlank()) return getWriteFormHtml("제목을 입력해주세요");
+        if(content.isBlank()) return getWriteFormHtml("내용을 입력해주세요");
+
         Post post = postService.write(title, content);
 
         return "%d번 글이 작성되었습니다.".formatted(post.getId());
