91a86176174259604469b125d9a72a2af741049c
woongcheol Na <32201345@dankook.ac.kr>
2025-10-07 13:59:14 +0900

    자바스크립트를 이용해 유효성체크시 포커싱 기능 보완

--- src/main/java/com/backend/domain/post/post/controller/PostController.java
+++ src/main/java/com/backend/domain/post/post/controller/PostController.java
@@ -15,7 +15,7 @@ public class PostController {
 
     private final PostService postService;
 
-    private String getWriteFormHtml(String errorMessage, String title, String content){
+    private String getWriteFormHtml(String errorMessage, String title, String content, String errorFieldName){
         return """
                 <div style="color:red">%s</div>
                 <form method="POST" action="http://localhost:8080/posts/doWrite">
@@ -25,14 +25,22 @@ public class PostController {
                   <br>
                   <input type="submit" value="작성">
                 </form>
-                """.formatted(errorMessage, title, content);
+                
+                <script>
+                    const errorFieldName = "%s";
+                    if(errorFieldName.length > 0) {
+                        const form = document.querySelector("form");
+                        form[errorFieldName].focus();
+                    }
+                </script>
+                """.formatted(errorMessage, title, content, errorFieldName);
     }
 
     @GetMapping("/posts/write")
     @ResponseBody
     public String write(){
 
-        return getWriteFormHtml("", "","");
+        return getWriteFormHtml("", "","", "title");
     }
 
     //POST 요청으로 받는다
@@ -43,8 +51,8 @@ public class PostController {
             String content
     ){
 
-        if(title.isBlank()) return getWriteFormHtml("제목을 입력해주세요", title, content);
-        if(content.isBlank()) return getWriteFormHtml("내용을 입력해주세요", title, content);
+        if(title.isBlank()) return getWriteFormHtml("제목을 입력해주세요", title, content,"title");
+        if(content.isBlank()) return getWriteFormHtml("내용을 입력해주세요", title, content, "content");
 
         Post post = postService.write(title, content);
 
