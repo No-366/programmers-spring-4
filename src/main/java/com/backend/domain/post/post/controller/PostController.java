06b8b06e5a712894706f8072daec13909b2bd0ba
woongcheol Na <32201345@dankook.ac.kr>
2025-10-07 17:26:03 +0900

    입력값을 모아서 @ModelAttribute을 사용한 객체로 받기, NotBlank와 Size, Validated를 이용한 유효성 체크 최적화

--- src/main/java/com/backend/domain/post/post/controller/PostController.java
+++ src/main/java/com/backend/domain/post/post/controller/PostController.java
@@ -2,12 +2,14 @@ package com.backend.domain.post.post.controller;
 
 import com.backend.domain.post.post.entity.Post;
 import com.backend.domain.post.post.service.PostService;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -43,22 +45,37 @@ public class PostController {
         return getWriteFormHtml("", "","", "title");
     }
 
+    @AllArgsConstructor
+    @Getter
+    public static class PostWriteForm{
+        @NotBlank
+        @Size(min = 1, max = 10)
+        private String title;
+
+        @NotBlank
+        @Size(min = 1, max = 100)
+        private String content;
+    }
+
     //POST 요청으로 받는다
     @PostMapping("/posts/doWrite")
     @ResponseBody
     public String doWrite(
-            String title,
-            String content
+            //다음은 프레임워크 규칙임
+            //@ModelAttribute("")은 생략 가능,
+            //PostWriteform 클래스이름의 첫번째 단어를 소문자로 바꿔서 매개값으로 사용
+        @Validated @ModelAttribute("postWriteForm") PostWriteForm form // PostWriteForm 클래스 형태로 변환해서 받겠다
     ){
 
-        if(title.isBlank()) return getWriteFormHtml("제목을 입력해주세요", title, content,"title");
-        if(title.length()<2) return getWriteFormHtml("제목은 2글자 이상 적어주세요.", title, content,"title");
-        if(title.length()>10) return getWriteFormHtml("제목은 10글자 이하로 적어주세요", title, content,"title");
-        if(content.isBlank()) return getWriteFormHtml("내용을 입력해주세요", title, content, "content");
-        if(content.length()<2) return getWriteFormHtml("내용은 2글자 이상 적어주세요.", title, content,"content");
-        if(content.length()>100) return getWriteFormHtml("내용은 100글자를 넘을 수 없습니다", title, content,"content");
+        // 아래처럼 일일이 여기에 작성하지 말고 NotBlank와 Size, Validated를 이용하자
+//        if(title.isBlank()) return getWriteFormHtml("제목을 입력해주세요", title, content,"title");
+//        if(title.length()<2) return getWriteFormHtml("제목은 2글자 이상 적어주세요.", title, content,"title");
+//        if(title.length()>10) return getWriteFormHtml("제목은 10글자 이하로 적어주세요", title, content,"title");
+//        if(content.isBlank()) return getWriteFormHtml("내용을 입력해주세요", title, content, "content");
+//        if(content.length()<2) return getWriteFormHtml("내용은 2글자 이상 적어주세요.", title, content,"content");
+//        if(content.length()>100) return getWriteFormHtml("내용은 100글자를 넘을 수 없습니다", title, content,"content");
 
-        Post post = postService.write(title, content);
+        Post post = postService.write(form.title, form.content);
 
         return "%d번 글이 작성되었습니다.".formatted(post.getId());
     }
